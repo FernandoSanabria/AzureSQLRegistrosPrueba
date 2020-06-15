7250aee9c83ebd132d71b94232b9d4949332b437
FernandoSanabria <FernandoSanabria@github.com>
2020-06-14 19:03:46 -0500

    setting connection  configuration to Azure SQL databese from ape net core (ContactContext was created and set)

--- ApiAzureSQLRegistrosPrueba/Startup.cs
+++ ApiAzureSQLRegistrosPrueba/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ApiAzureSQLRegistrosPrueba.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiAzureSQLRegistrosPrueba
 {
@@ -25,6 +27,8 @@ namespace ApiAzureSQLRegistrosPrueba
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ContactsContext>(opt => opt.UseSqlServer(connectionString));
             services.AddControllers();
         }
 
