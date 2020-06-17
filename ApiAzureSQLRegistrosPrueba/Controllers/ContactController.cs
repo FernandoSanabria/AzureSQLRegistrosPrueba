8ae7632b50d6924d464db7b51f265c64feae47b7
FernandoSanabria <FernandoSanabria@github.com>
2020-06-16 20:25:26 -0500

    webapi finished

--- ApiAzureSQLRegistrosPrueba/Controllers/ContactController.cs
+++ ApiAzureSQLRegistrosPrueba/Controllers/ContactController.cs
@@ -22,6 +22,49 @@ namespace ApiAzureSQLRegistrosPrueba.Controllers
             return contactsContext.ContactSet.ToList();
         }
 
+          // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Contacts> Get(int id)
+        {
+            var selectedContact = (from c in contactsContext.ContactSet
+                                    where c.Identificador == id
+                                    select c).FirstOrDefault();
+            return selectedContact;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] Contacts value)
+        {
+            Contacts newContact = value;
+            contactsContext.ContactSet.Add(newContact);
+            contactsContext.SaveChanges();
+            return Ok("Tu contacto ha sido agregado");
+        }
+
+        // // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Contacts value)
+        {
+            Contacts updatedContact = value;
+            var selectedElement = contactsContext.ContactSet.Find(updatedContact.Identificador);
+            selectedElement.Nombre = value.Nombre;
+            selectedElement.Email = value.Email;
+            contactsContext.SaveChanges();
+        }
+
+        // // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var selectedElement = contactsContext.ContactSet.Find(id);
+            contactsContext.ContactSet.Remove(selectedElement);
+            contactsContext.SaveChanges();
+            return Ok("Tu contacto ha sido eliminado");
+        }
+
+
+
         ~ContactController() 
         {
             contactsContext.Dispose();
